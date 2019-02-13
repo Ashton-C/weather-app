cd8abe2186464bc5675004efe4ee9fee59b18ed7
Ashton Christensen <armadillomaster@gmail.com>
2019-02-13 16:24:57 -0700

    Started API implementation
    started working with ipstack for geolocation via ip and darksky for weather data. so far still broken... hoep to fix tonight.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,37 @@ import SmView from "./components/smview/SmView";
 import Footer from "./components/footer/Footer";
 import "./App.css";
 
+let ds_api_key = process.env.DARK_SKY_SECRET;
+let ip_api_key = process.env.IPSTACK_SECRET;
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.generateOneDay = this.generateOneDay.bind(this);
+    this.generateTenDay = this.generateTenDay.bind(this);
     this.state = {
       today: new Date(),
-      tomorrow: this.state.today.setDate(getDate() + 1)
+      tomorrow: null
     };
   }
-
+  generateOneDay = () => {
+    let location = fetch(
+      `api.ipstack.com/check?access_key=${ip_api_key}&output=json&fields=longitude,latitude`
+    )
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(data => {
+        let longitude = data.longitude;
+        let latitude = data.latitude;
+        console.log(longitude);
+        return [latitude, longitude];
+      });
+    let url = `https://api.darksky.net/forecast/${ds_api_key}/${location[0]},${
+      location[1]
+    }`;
+  };
   generateTenDay = () => {
     let tenD = [];
     let date = this.state.today;
@@ -25,6 +47,7 @@ class App extends Component {
   };
 
   render() {
+    let test = this.generateOneDay();
     return (
       <div className="App">
         <Header />
