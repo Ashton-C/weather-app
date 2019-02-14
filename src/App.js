77e7de595f35583f15963abe7c5798bf8901239b
ashton_c <armadillomaster@gmail.com>
2019-02-14 00:28:47 -0700

    More API work!
    Finally made the APIs work! Now I need to process the JSON.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,60 @@
-import React, { Component } from "react";
-import Header from "./components/header/Header";
-import LgView from "./components/lgview/LgView";
-import SmView from "./components/smview/SmView";
-import Footer from "./components/footer/Footer";
-import "./App.css";
-
-let ds_api_key = process.env.DARK_SKY_SECRET;
-let ip_api_key = process.env.IPSTACK_SECRET;
+import React, { Component } from 'react';
+import Header from './components/header/Header';
+import LgView from './components/lgview/LgView';
+import SmView from './components/smview/SmView';
+import Footer from './components/footer/Footer';
+import './App.css';
+import secrets from './secrets';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.getUrl = this.getUrl.bind(this);
     this.generateOneDay = this.generateOneDay.bind(this);
     this.generateTenDay = this.generateTenDay.bind(this);
     this.state = {
+      url: null,
       today: new Date(),
       tomorrow: null
     };
   }
-  generateOneDay = () => {
-    let location = fetch(
-      `api.ipstack.com/check?access_key=${ip_api_key}&output=json&fields=longitude,latitude`
+  getUrl = () => {
+    let url = fetch(
+      `http://api.ipstack.com/check?access_key=${ip_api_key}&output=json&fields=longitude,latitude`
     )
       .then(response => {
-        console.log(response);
         return response.json();
       })
       .then(data => {
         let longitude = data.longitude;
         let latitude = data.latitude;
-        console.log(longitude);
         return [latitude, longitude];
+      })
+      .then(location => {
+        let url = `https://api.darksky.net/forecast/${darksky_secret}/${
+          location[0]
+        },${location[1]}`;
+        return url;
       });
-    let url = `https://api.darksky.net/forecast/${ds_api_key}/${location[0]},${
-      location[1]
-    }`;
+    this.setState({ url: [url] });
+    console.log(url);
+    return url;
+  };
+
+  async getData(url) {
+    let data = fetch(await url).then(res => {
+      console.log(res.json());
+      return res;
+    });
+  }
+  generateOneDay = () => {
+    let tenD = [];
+    let date = this.state.today;
+    for (let i = 0; i < 10; i++) {
+      tenD.append(<Sm-View content={date} />);
+      date.setDate(date.getDate() + 1);
+      console.log(date);
+    }
   };
   generateTenDay = () => {
     let tenD = [];
@@ -46,8 +66,11 @@ class App extends Component {
     }
   };
 
+  componentDidMount() {
+    let data = this.getData(this.getUrl());
+  }
+
   render() {
-    let test = this.generateOneDay();
     return (
       <div className="App">
         <Header />
